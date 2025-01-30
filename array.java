7bdb0279c7e750609e1be5394e0eeefbe9eed464
ayogitha <eliteresourcenetwork@gmail.com>
2025-01-30 22:33:11 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("The elements of the array are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+             scanner.close();
+    }
+
+    
+}
